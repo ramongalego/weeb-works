5bbea66ba0670a83ad36b948c04dacbb61320005
Ramon Galego <ramongalegocs@gmail.com>
2023-10-26 03:09:03 +0100

    Adds min height to AnimeDetailsTrailer and AnimeDetailsInfo / Sanitises trailer url to remove autoplay

--- src/features/anime/AnimeDetails/AnimeDetailsTrailer.jsx
+++ src/features/anime/AnimeDetails/AnimeDetailsTrailer.jsx
@@ -1,12 +1,14 @@
 const AnimeDetailsTrailer = ({ trailerUrl }) => {
+  const sanitizedTrailerUrl = trailerUrl.replace('autoplay=1', 'autoplay=0');
+
   return (
-    <div className='rounded bg-white p-4 pb-6'>
+    <div className='min-h-[600px] rounded bg-white p-4'>
       <h1 className='text-xl font-semibold'>Trailer</h1>
       <div className='mt-6 flex h-full w-full justify-center'>
         <iframe
           className='w-full rounded'
-          height='500'
-          src={trailerUrl}
+          height='510'
+          src={sanitizedTrailerUrl}
           title='YouTube Trailer Player'
           allow='accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture'
           allowFullScreen
