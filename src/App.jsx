acf2e1a2110e264275da4f97124133f131030e25
Ramon Galego <ramongalegocs@gmail.com>
2023-10-31 01:39:26 +0000

    Adds Pages component to clean up App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 import useUserStore from './app/store';
 import Navbar from './components/Navbar';
-import PrivateRoutes from './components/PrivateRoutes';
 import ScrollToTop from './components/ScrollToTop';
-import { AnimeDetails } from './features/anime/AnimeDetails';
-import Browse from './pages/Browse';
-import Home from './pages/Home';
-import Login from './pages/Login';
-import NotFound from './pages/NotFound';
-import Profile from './pages/Profile';
-import SignUp from './pages/SignUp';
+import Pages from './pages/Pages';
 
 const queryClient = new QueryClient();
 
@@ -30,20 +23,7 @@ const App = () => {
       <Navbar />
       <main className='mb-10 tracking-wide text-gray-800'>
         <QueryClientProvider client={queryClient}>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/anime' element={<Browse />} />
-            <Route path='/anime/:filter' element={<Browse />} />
-            <Route path='/anime/details/:id' element={<AnimeDetails />} />
-            <Route path='*' element={<NotFound />} />
-
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/login' element={<Login />} />
-
-            <Route element={<PrivateRoutes />}>
-              <Route path='/user/:username' element={<Profile />} />
-            </Route>
-          </Routes>
+          <Pages />
           <ReactQueryDevtools />
         </QueryClientProvider>
       </main>
---
+++ src/pages/Pages.jsx
@@ -0,0 +1,32 @@
+import { Routes, Route } from 'react-router-dom';
+
+import PrivateRoutes from '../components/PrivateRoutes';
+import { AnimeDetails } from '../features/anime/AnimeDetails';
+
+import Browse from './Browse';
+import Home from './Home';
+import Login from './Login';
+import NotFound from './NotFound';
+import Profile from './Profile';
+import SignUp from './SignUp';
+
+const Pages = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/anime' element={<Browse />} />
+      <Route path='/anime/:filter' element={<Browse />} />
+      <Route path='/anime/details/:id' element={<AnimeDetails />} />
+      <Route path='*' element={<NotFound />} />
+
+      <Route path='/signup' element={<SignUp />} />
+      <Route path='/login' element={<Login />} />
+
+      <Route element={<PrivateRoutes />}>
+        <Route path='/user/:username' element={<Profile />} />
+      </Route>
+    </Routes>
+  );
+};
+
+export default Pages;
