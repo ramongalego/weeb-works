4c58130d011cbe260efd24a09eab6d03e59ec550
Ramon Galego <ramongalegocs@gmail.com>
2023-11-01 00:21:28 +0000

    Adds Appwrite key to env file / Adds .env to gitignore

--- src/appwriteConfig.js
+++ src/appwriteConfig.js
@@ -2,7 +2,7 @@ import { Client, Account } from 'appwrite';
 
 export const client = new Client();
 
-client.setEndpoint('https://cloud.appwrite.io/v1').setProject('653ff0db331dd5037cbf');
+client.setEndpoint('https://cloud.appwrite.io/v1').setProject(import.meta.env.VITE_APPWRITE_KEY);
 
 export const account = new Account(client);
 export { ID } from 'appwrite';
